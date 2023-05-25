57973e2864aa1f09d63dd6d2bc0f36680649370a
ayakamada <sdkamada@gmail.com>
2023-05-25 20:07:54 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,22 @@
-import Image from 'next/image'
-import {fetchPages} from '@/lib/notion'
+import Image from "next/image";
+import { fetchPages } from "@/lib/notion";
+import Link from "next/link";
 
 type Post = /*unresolved*/ any;
 
 export default function Home({ posts }: { posts: Post[] }) {
-  
+  console.log(posts);
   return (
-    <main
-      className={` `}
-    >
-
+    <main className="">
+      {posts.map((post) => (
+        <Link key={post.id} href={post.id}>
+          <h1>{post.properties.Post.title[0].plain_text}</h1>
+          <p>{post.properties.Description.rich_text[0].plain_text}</p>
+        </Link>
+      ))}
     </main>
-  )
+  );
 }
-
 
 export const getServerSideProps = async () => {
   const posts = await fetchPages();
@@ -22,8 +25,7 @@ export const getServerSideProps = async () => {
 
   return {
     props: {
-      posts: posts,
+      posts: posts.results,
     },
   };
 };
-
