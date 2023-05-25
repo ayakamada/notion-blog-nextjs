bc4f2a65480457a513694ab7c3546371f911ea07
ayakamada <sdkamada@gmail.com>
2023-05-25 20:07:27 +0900

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="mt-16 flex flex-col items-center">
+        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>aya kamada</div>
+          <div>{` • `}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <Link href="/">Nextjs + Notion BLOG</Link>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">Tailwind Nextjs Theme</Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
