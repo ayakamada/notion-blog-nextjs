1cb62e913a41b5db5c1bc3afc85552244e2e3562
ayakamada <sdkamada@gmail.com>
2023-05-28 09:28:02 +0900

    layout

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -1,45 +1,41 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Link from "next/link";
-// import MobileNav from "./MobileNav";
-// import ThemeSwitch from "./ThemeSwitch";
 
-const headerNavLinks = [
-  { href: "/blog", title: "Blog" },
-  // { href: "/tags", title: "Tags" },
-  // { href: "/projects", title: "Projects" },
-  // { href: "/about", title: "About" },
-];
+import SectionContainer from "@/components/SectionContainer";
+import headerNavLinks from "@/data/headerNavLinks";
 
 const LayoutWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex h-screen flex-col justify-between">
-      <header className="flex items-center justify-between py-10">
-        <div>
-          <Link href="/" aria-label="Nextjs + Notion BLOG">
-            <div className="flex items-center justify-between">
-              <div className="mr-3">{/* <Logo /> */}</div>
-              Nextjs + Notion BLOG
-            </div>
-          </Link>
-        </div>
-        <div className="flex items-center text-base leading-5">
-          <div className="hidden sm:block">
-            {headerNavLinks.map((link) => (
-              <Link
-                key={link.title}
-                href={link.href}
-                className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-              >
-                {link.title}
-              </Link>
-            ))}
+    <SectionContainer>
+      <div className="flex h-screen flex-col justify-between">
+        <header className="flex items-center justify-between py-10">
+          <div>
+            <Link href="/" aria-label="Nextjs + Notion BLOG">
+              <div className="flex items-center justify-between">
+                <div className="mr-3">{/* <Logo /> */}</div>
+                Nextjs + Notion BLOG
+              </div>
+            </Link>
           </div>
-        </div>
-      </header>
-      <main className="mb-auto">{children}</main>
-      <Footer />
-    </div>
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </header>
+        <main className="mb-auto">{children}</main>
+        <Footer />
+      </div>
+    </SectionContainer>
   );
 };
 
