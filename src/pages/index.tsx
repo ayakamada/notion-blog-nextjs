e269dff70d2732f7035390787720e104dcfa42e6
ayakamada <sdkamada@gmail.com>
2023-05-28 10:08:33 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,38 +10,35 @@ export default function Home({ posts }: { posts: Post[] }) {
     <main className="">
       <ul>
         {posts.map((post, i) => (
-          <Link key={post.id} href={`/blog/${post.properties.Slug.rich_text[0].plain_text}`}>
-            <li key={i} className="py-4">
-              <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                <dl>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={post.properties.Date.datestart}>{formatDate(post.properties.Date.date.start)}</time>
-                  </dd>
-                </dl>
-                <div className="space-y-3 xl:col-span-3">
-                  <div>
+          <li key={i} className="py-4">
+            <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+              <dl>
+                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  <time dateTime={post.properties.Date.datestart}>{formatDate(post.properties.Date.date.start)}</time>
+                </dd>
+              </dl>
+              <div className="space-y-3 xl:col-span-3">
+                <div>
+                  <Link
+                    href={`/blog/${post.properties.Slug.rich_text[0].plain_text}`}
+                    className="text-gray-900 dark:text-gray-100"
+                  >
                     <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                      <Link
-                        href={`/blog/${post.properties.Slug.rich_text[0].plain_text}`}
-                        className="text-gray-900 dark:text-gray-100"
-                      >
-                        {post.properties.Post.title[0].plain_text}
-                      </Link>
+                      {post.properties.Post.title[0].plain_text}
                     </h3>
-                    {/* <div className="flex flex-wrap">
+                  </Link>
+                  {/* <div className="flex flex-wrap">
                     {tags.map((tag) => (
                       <Tag key={tag} text={tag} />
                     ))}
                   </div> */}
-                  </div>
-                  <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                    {post.properties.Description.rich_text[0].plain_text}
-                  </div>
                 </div>
-              </article>
-            </li>
-          </Link>
+                <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                  {post.properties.Description.rich_text[0].plain_text}
+                </div>
+              </div>
+            </article>
+          </li>
         ))}
       </ul>
     </main>
