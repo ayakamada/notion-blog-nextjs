aa3931d5cf094ce14f345c2aab940ed127052dba
ayakamada <sdkamada@gmail.com>
2023-06-07 16:59:11 +0900

    Create notion-page.ts

---
+++ src/lib/types/notion-page.ts
@@ -0,0 +1,18 @@
+export interface INotionPage {
+  object: string;
+  id: string;
+  created_time: string;
+  last_edited_time: string;
+  parent: {
+    type: string;
+    database_id: string;
+  };
+  archived?: boolean;
+  properties: {
+    Page: any;
+    Slug: any;
+    Description: any;
+    Date: any;
+    Published: any;
+  };
+}
