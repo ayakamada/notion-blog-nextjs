2215174786a0aadfc6d37c2e358e6875d7878442
ayakamada <sdkamada@gmail.com>
2023-06-07 16:59:14 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,17 @@ import type { AppProps } from "next/app";
 
 import { ThemeProvider } from "next-themes";
 
-import {siteMetadata} from "@/data/siteMetadata";
+import { siteMetadata } from "@/data/siteMetadata";
 import LayoutWrapper from "@/components/LayoutWrapper";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-       </ThemeProvider>
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </ThemeProvider>
     </>
   );
 }
