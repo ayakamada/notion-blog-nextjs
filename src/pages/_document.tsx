4c30ff7693c618a3d6f4713ce66d91db9aa0e3d1
ayakamada <sdkamada@gmail.com>
2023-06-07 16:59:17 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/styles/atom-one-dark.min.css"
+        />
+        <script
+          src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/highlight.min.js"
+          async={false}
+          defer={false}
+        ></script>
+        <script>hljs.initHighlightingOnLoad();</script>
+      </Head>
       <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
         <Main />
         <NextScript />
